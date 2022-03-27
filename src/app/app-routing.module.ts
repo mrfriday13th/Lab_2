4807938aa99901b4373a10006152106d1bce05f8
mrfriday13th <doantnph18167@fpt.edu.vn>
2022-03-27 15:24:17 +0700

    up

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,58 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductComponent } from './product/product.component';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
+import { StudentFormComponent } from './student-form/student-form.component';
+import { StudentComponent } from './student/student.component';
+import { UserComponent } from './user/user.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { AsideComponent } from './layout/aside/aside.component'
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'product',
+    component: ProductComponent
+  },
+  {
+    path: 'product/:id',
+    component: ProductDetailComponent
+  },
+  {
+    path: 'user',
+    component: UserComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'students',
+    component: StudentComponent
+  },
+  {
+    path: 'students/form',
+    component: StudentFormComponent
+  },
+  {
+    path: 'students/form/:id',
+    component: StudentFormComponent
+  },
+  {
+    path: 'students/:id',
+    component: StudentDetailComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { ProductComponent } from './product/product.component';
+// import thanh phan form
+import { FormsModule } from '@angular/forms';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { StudentComponent } from './student/student.component';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
+import { StudentFormComponent } from './student-form/student-form.component';
+import { StudentService } from './services/student.service';
+import { HeaderComponent } from './layout/header/header.component';
+import { AsideComponent } from './layout/aside/aside.component';
+import { FooterComponent } from './layout/footer/footer.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    UserComponent,
+    ProductComponent,
+    ProductDetailComponent,
+    StudentComponent,
+    StudentDetailComponent,
+    StudentFormComponent,
+    HeaderComponent,
+    AsideComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FormsModule, // dua vao de cac component con co the sd,
+    HttpClientModule
+  ],
+  providers: [StudentService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
